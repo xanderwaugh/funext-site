7ef8fd0a6b253f4f81442ddc268eabf4b773d168
xandykandy <xandykandy@gmail.com>
2022-01-21 02:46:10 -0500

    master

---
+++ src/pages/notes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+
+const NotesPage: React.FC = () => {
+    return (
+        <Box minH="90vh">
+            <Heading>Coming Soon . . . </Heading>
+        </Box>
+    );
+};
+
+export default NotesPage;
